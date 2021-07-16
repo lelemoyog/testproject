f44c2dd6e94cda623005c004005d8a920787eb6d
saitemu_issa <saitemu.issa@student.moringaschool.com>
2021-07-16 06:03:20 +0300

    the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { GoalComponent } from './goal/goal.component';
+import { GoaldetailComponent } from './goal/goaldetail/goaldetail.component';
+import { StrikethroughDirective } from './strikethrough.directive';
+import { DateCountPipe } from './date-count.pipe';
+import { GoalFormComponent } from './goal-form/goal-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    GoalComponent,
+    GoaldetailComponent,
+    StrikethroughDirective,
+    DateCountPipe,
+    GoalFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/goal/goaldetail/goaldetail.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit,Input ,Output,EventEmitter} from '@angular/core';
+import { Goal } from '../../goal';
+@Component({
+  selector: 'app-goaldetail',
+  templateUrl: './goaldetail.component.html',
+  styleUrls: ['./goaldetail.component.css']
+})
+export class GoaldetailComponent implements OnInit {
+  @Input() 'goal': Goal;
+  @Output() isComplete = new EventEmitter<boolean>();
+
+  goalComplete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
+  goalDelete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
